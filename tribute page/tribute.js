b80be733326a251fc0aad8fd2f64fdd06311230e
Morenike Oyewole <65955563+HerRoyalPianist@users.noreply.github.com>
2020-11-05 12:00:57 +0100

    Add files via upload

---
+++ tribute page/tribute.js
@@ -0,0 +1,6 @@
+function myFunction() {
+              document.getElementById("demo").innerHTML="Paragraph changed.";
+              document.getElementById("test").style.fontSize="30px";
+            }
+
+
